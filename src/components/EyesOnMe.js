2b317f12857bfcebd946f01465174b4e8c8ab9d6
hagee <hagee.ktr@outlook.com>
2025-04-11 13:19:08 +0300

    Add focus and blur listeners to DOM elements

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function handleFocus() {
+        console.log('Good!')
+    }
+    function handleBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    )
+    
+}
+export default EyesOnMe;
